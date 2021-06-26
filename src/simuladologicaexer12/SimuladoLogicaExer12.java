bab673ac017cf3f8064592cbebf705fdceb54194
Davi de Oliveira <odavi076@gmail.com>
2021-06-26 16:07:21 -0300

    Simulado Lógica Exercício 12 - 26/06/21

---
+++ src/simuladologicaexer12/SimuladoLogicaExer12.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladologicaexer12;
+
+/**
+ *
+ * @author odavi
+ */
+public class SimuladoLogicaExer12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int numero = 2; numero < 100; numero = numero + 2){
+            System.out.println("Número: "+numero);
+        }
+    }
+    
+}
